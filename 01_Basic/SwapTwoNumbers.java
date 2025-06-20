5b76c6ff621061412119327889bda47696205771
shanizala <132726720+shanizala@users.noreply.github.com>
2025-06-21 01:04:16 +0530

    Add files via upload

---
+++ 01_Basic/SwapTwoNumbers.java
@@ -0,0 +1,28 @@
+// File: SwapTwoNumbers.java
+// Topic: Basic
+// Author: Z. Shani
+// Description: Swaps two numbers using a temporary variable
+
+import java.util.Scanner;
+
+public class SwapTwoNumbers 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number :: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number :: ");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+
+        sc.close();
+    }
+}
