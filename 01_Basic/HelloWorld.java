3f0974dd256fbe8c23636f0e08864846a0876b10
shanizala <132726720+shanizala@users.noreply.github.com>
2025-06-20 11:58:53 +0530

    Create HelloWorld.java

---
+++ 01_Basic/HelloWorld.java
@@ -0,0 +1,13 @@
+// File: HelloWorld.java
+// Topic: Basic
+// Author: Z. Sunny
+// Description: Prints "Hello, World!" to console
+
+public class HelloWorld
+{
+  public static void main(String [] args )
+  {
+    System.out.println("Hello , World!");
+  }
+}
+    
