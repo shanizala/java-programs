116cefdffbe6ed7098e9e60788d3bd553e86ed34
shanizala <132726720+shanizala@users.noreply.github.com>
2025-06-21 00:36:39 +0530

    Add files via upload

---
+++ 01_Basic/EvenOdd.java
@@ -0,0 +1,18 @@
+// File: EvenOdd.java
+// Topic: Basic
+// Author: Z. Sunny
+// Description: Checks whether a number is even or odd
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
